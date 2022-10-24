cce6b888c2e7f43b26c9c6210ad2abc00ddf3f13
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-10-24 14:24:23 +0530

    minor changes to user profile screen

--- lib/pages/profileUser.dart
+++ lib/pages/profileUser.dart
@@ -19,59 +19,62 @@ class _profileUserState extends State<profileUser> {
   @override
   Widget build(BuildContext context) {
     data = data.isNotEmpty? data: ModalRoute.of(context)?.settings.arguments as Map;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white24,
-        title: Text("User Profile",style: GoogleFonts.poppins(color: Colors.white),),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
-          ),
-          onPressed: (){
-            Navigator.of(context).pushNamed(chatScreen.id);
-          },
-        ),
-
-      ),
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FutureBuilder<DocumentSnapshot>(
-              future: _fireStore.collection('users').doc(data['senderUid']).get(),
-                builder: (BuildContext context,AsyncSnapshot<DocumentSnapshot> snapshot){
-                  if (snapshot.hasError) {
-                    return Text("Something went wrong");
-                  }
-
-                  if (snapshot.hasData && !snapshot.data!.exists) {
-                    return Text("Document does not exist");
-                  }
-
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          radius: 60,
-                          backgroundColor: Colors.white,
-                          backgroundImage: NetworkImage(data['profileImgLink']),
-                        ),
-                        const SizedBox(height: 30,),
-                        Text("Email:${data["email"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
-                        Text("User name:${data["UserName"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
-                        Text("Phone number:${data["PhoneNumber"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
-                        Text("About:${data["about"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
-                      ],
-                    );
-                  }
-                  return Text("loading",style: TextStyle(color: Colors.white),);
-                }
+    return WillPopScope(
+      onWillPop: ()async=>false,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white24,
+          title: Text("User Profile",style: GoogleFonts.poppins(color: Colors.white),),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
             ),
-          ],
+            onPressed: (){
+              Navigator.of(context).pushNamed(chatScreen.id);
+            },
+          ),
+
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FutureBuilder<DocumentSnapshot>(
+                future: _fireStore.collection('users').doc(data['senderUid']).get(),
+                  builder: (BuildContext context,AsyncSnapshot<DocumentSnapshot> snapshot){
+                    if (snapshot.hasError) {
+                      return Text("Something went wrong");
+                    }
+
+                    if (snapshot.hasData && !snapshot.data!.exists) {
+                      return Text("Document does not exist");
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundColor: Colors.white,
+                            backgroundImage: NetworkImage(data['profileImgLink']),
+                          ),
+                          const SizedBox(height: 30,),
+                          Text("Email:${data["email"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
+                          Text("User name:${data["UserName"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
+                          Text("Phone number:${data["PhoneNumber"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
+                          Text("About:${data["about"]}",style:const TextStyle(color: Colors.white,fontSize: 20)),
+                        ],
+                      );
+                    }
+                    return Text("loading",style: TextStyle(color: Colors.white),);
+                  }
+              ),
+            ],
+          ),
         ),
       ),
     );
